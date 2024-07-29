741433a21ba54812c8096e3cd020d0e4b3d85b6a
Bandar-Zuhair <146974861+Bandar-Zuhair@users.noreply.github.com>
2024-07-30 00:38:30 +0700

    Update create-new-package-insert-data.js

--- create-new-package-insert-data.js
+++ create-new-package-insert-data.js
@@ -2869,6 +2869,7 @@ let isVisible = function (element) {
     return element && element.style.display !== 'none' && element.offsetParent !== null;
 };
 
+/* Download the pdf file with the given name */
 downloadPdfWithCustomName = async function (pdfName) {
     let { jsPDF } = window.jspdf;
 
@@ -2941,10 +2942,10 @@ downloadPdfWithCustomName = async function (pdfName) {
 
         let pdf = new jsPDF('p', 'mm', [pdfWidth, pdfHeight]);
 
-        let imgData = combinedCanvas.toDataURL('image/png', 1.0); // Use PNG format for higher quality
+        let imgData = combinedCanvas.toDataURL('image/jpeg', 0.7); // Adjust JPEG quality for better balance
 
         // Add the image to the PDF with padding
-        pdf.addImage(imgData, 'PNG', padding, 0, contentWidth, pdfHeight, '', 'FAST');
+        pdf.addImage(imgData, 'JPEG', padding, 0, contentWidth, pdfHeight, '', 'FAST');
         pdf.save(pdfName);
 
         // Hide all elements with the class name after saving the PDF
@@ -3026,4 +3027,3 @@ downloadPdfWithCustomName = async function (pdfName) {
 
     await processSections(sections, scale); // Process visible sections to generate the PDF
 };
-
