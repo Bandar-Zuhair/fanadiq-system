6063ccc27b8bbee5ef945b9daef14474320e307b
Bandar-Zuhair <146974861+Bandar-Zuhair@users.noreply.github.com>
2024-07-31 01:10:44 +0700

    Update create-new-package-insert-data.js

--- create-new-package-insert-data.js
+++ create-new-package-insert-data.js
@@ -3026,7 +3026,7 @@ downloadPdfWithCustomName = async function (pdfName) {
         }
     });
 
-    // Increase the scale specifically for mobile users
+    // Determine the scale based on the device type
     let scale = /Mobi|Android/i.test(navigator.userAgent) ? 2 : 2; // Higher scale for mobile devices
 
     await processSections(sections, scale); // Process visible sections to generate the PDF
