8e61f12f88f030db69eb15092a562742facd43a7
Bandar-Zuhair <146974861+Bandar-Zuhair@users.noreply.github.com>
2024-08-08 03:53:57 +0700

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -3,8 +3,12 @@ import bodyParser from 'body-parser';
 import fetch from 'node-fetch';
 import cors from 'cors';
 
+
 const app = express();
-const port = process.env.PORT || 3000; // Use the port provided by Repl.it
+const port = process.env.PORT || 3000;
+
+// The rest of your code...
+
 
 // Use CORS middleware
 app.use(cors({
@@ -37,5 +41,5 @@ app.post('/api/handleRequest', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Proxy server listening at https://b1d72741-6c12-4316-a0dc-897de7e0a31d-00-1flammxb2btfj.pike.replit.dev/`);
+    console.log(`Proxy server listening at http://localhost:${port}`);
 });
