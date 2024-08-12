b1b9db425ed0c74c4b4faa64831276cfb7ed48ba
Bandar-Zuhair <146974861+Bandar-Zuhair@users.noreply.github.com>
2024-08-12 14:50:18 +0700

    Add files via upload

---
+++ create-new-package-all-clint-visiting-places.js
@@ -0,0 +1,345 @@
+
+
+
+
+let allClintVisitingPlacesArray = [
+
+
+    /* Kuta object[0] */
+    {
+
+        honeymoon: {
+            visitingPlaceNamesDay1: [''],
+            visitingPlaceNamesDay2: ['التجديف في نهر تلاقاوجا + فعالية ركوب الخيل على الشاطئ الاسود'],
+            visitingPlaceNamesDay3: ['زيارة شاطئ The Beach Love والجلوس في الكافي المطل + زيارة شاطئ جمبران والجلوس في احد المطاعم ومشاهدة الغروب الجميل'],
+            visitingPlaceNamesDay4: ['زيارة شاطئ اطلالة الجرف الصخري ULUWATU + زيارة مولات كوتا (ديسكفوي + ليبو مول + بيتش ووك مول)'],
+            visitingPlaceNamesDay5: ['زيارة شاطئ PANTAI PANDAWA + زيارة ملاهي ترانس ستوديو'],
+            visitingPlaceNamesDay6: ['زيارة جزيرة NUSA PENDIA المشهورة بشواطئها الجميلة وتكون الرحلة بالقوارب ويتم زيارة 3 الى 4 شواطئ شاملة التوصيل من والى الفندق والمواصلات داخل الجزيرة'],
+            visitingPlaceNamesDay7: ['زيارة فعالية بحرية DOLPIN WATERSPORT + زيارة احد اجمل المعالم قرية جاتلوية المشهورة بمدرجات الأرز'],
+            visitingPlaceNamesDay8: ['يوم مفتوج للراحة والتسوق'],
+            visitingPlaceNamesDay9: ['الالعاب البحرية في منطقة نوسا دوا + الالعاب المائية WATERBOM'],
+        },
+
+
+        guys: {
+            visitingPlaceNamesDay1: [''],
+            visitingPlaceNamesDay2: ['الالعاب البحرية في منطقة نوسا دوا + الالعاب المائية WATERBOM'],
+            visitingPlaceNamesDay3: ['زيارة شاطئ PANTAI PANDAWA + زيارة ملاهي ترانس ستوديو'],
+            visitingPlaceNamesDay4: ['التجديف في نهر تلاقاوجا + فعالية ركوب الخيل على الشاطئ الاسود'],
+            visitingPlaceNamesDay5: ['زيارة جزيرة NUSA PENDIA المشهورة بشواطئها الجميلة وتكون الرحلة بالقوارب ويتم زيارة 3 الى 4 شواطئ شاملة التوصيل من والى الفندق والمواصلات داخل الجزيرة'],
+            visitingPlaceNamesDay6: ['زيارة شاطئ اطلالة الجرف الصخري ULUWATU + زيارة مولات كوتا (ديسكفوي + ليبو مول + بيتش ووك مول)'],
+            visitingPlaceNamesDay7: ['زيارة شاطئ The Beach Love والجلوس في الكافي المطل + زيارة شاطئ جمبران والجلوس في احد المطاعم ومشاهدة الغروب الجميل'],
+            visitingPlaceNamesDay8: ['زيارة فعالية بحرية DOLPIN WATERSPORT + زيارة احد اجمل المعالم قرية جاتلوية المشهورة بمدرجات الأرز'],
+            visitingPlaceNamesDay9: ['يوم مفتوج للراحة والتسوق'],
+        },
+
+
+        family: {
+            visitingPlaceNamesDay1: [''],
+            visitingPlaceNamesDay2: ['التجديف في نهر تلاقاوجا + فعالية ركوب الخيل على الشاطئ الاسود'],
+            visitingPlaceNamesDay3: ['زيارة شاطئ PANTAI PANDAWA + زيارة ملاهي ترانس ستوديو'],
+            visitingPlaceNamesDay4: ['زيارة جزيرة NUSA PENDIA المشهورة بشواطئها الجميلة وتكون الرحلة بالقوارب ويتم زيارة 3 الى 4 شواطئ شاملة التوصيل من والى الفندق والمواصلات داخل الجزيرة'],
+            visitingPlaceNamesDay5: ['الالعاب البحرية في منطقة نوسا دوا + الالعاب المائية WATERBOM'],
+            visitingPlaceNamesDay6: ['زيارة شاطئ The Beach Love والجلوس في الكافي المطل + زيارة شاطئ جمبران والجلوس في احد المطاعم ومشاهدة الغروب الجميل'],
+            visitingPlaceNamesDay7: ['زيارة شاطئ اطلالة الجرف الصخري ULUWATU + زيارة مولات كوتا (ديسكفوي + ليبو مول + بيتش ووك مول)'],
+            visitingPlaceNamesDay8: ['زيارة فعالية بحرية DOLPIN WATERSPORT + زيارة احد اجمل المعالم قرية جاتلوية المشهورة بمدرجات الأرز'],
+            visitingPlaceNamesDay9: ['يوم مفتوج للراحة والتسوق'],
+        },
+
+
+        twopeople: {
+            visitingPlaceNamesDay1: [''],
+            visitingPlaceNamesDay2: ['التجديف في نهر تلاقاوجا + فعالية ركوب الخيل على الشاطئ الاسود'],
+            visitingPlaceNamesDay3: ['زيارة شاطئ PANTAI PANDAWA + زيارة ملاهي ترانس ستوديو'],
+            visitingPlaceNamesDay4: ['الالعاب البحرية في منطقة نوسا دوا + الالعاب المائية WATERBOM'],
+            visitingPlaceNamesDay5: ['زيارة جزيرة NUSA PENDIA المشهورة بشواطئها الجميلة وتكون الرحلة بالقوارب ويتم زيارة 3 الى 4 شواطئ شاملة التوصيل من والى الفندق والمواصلات داخل الجزيرة'],
+            visitingPlaceNamesDay6: ['زيارة شاطئ اطلالة الجرف الصخري ULUWATU + زيارة مولات كوتا (ديسكفوي + ليبو مول + بيتش ووك مول)'],
+            visitingPlaceNamesDay7: ['زيارة شاطئ The Beach Love والجلوس في الكافي المطل + زيارة شاطئ جمبران والجلوس في احد المطاعم ومشاهدة الغروب الجميل'],
+            visitingPlaceNamesDay8: ['زيارة فعالية بحرية DOLPIN WATERSPORT + زيارة احد اجمل المعالم قرية جاتلوية المشهورة بمدرجات الأرز'],
+            visitingPlaceNamesDay9: ['يوم مفتوج للراحة والتسوق'],
+        },
+
+    },
+
+
+    /* Ubud object[1] */
+    {
+
+        honeymoon: {
+            visitingPlaceNamesDay1: [''],
+            visitingPlaceNamesDay2: ['زيارة بحيرة براتان الفاتنة واخذ جولة بالقارب + زيارة شمال الجزيرة المعروفة بأجوائها الساحرة'],
+            visitingPlaceNamesDay3: ['زيارة منتزه ALOHA مراجيح الهواء + سنتر اوبود للغدا حسب الرغبة'],
+            visitingPlaceNamesDay4: ['زيارة بوابة منتجع هندارا الشهيرة + زيارة بحيرتين التوام'],
+            visitingPlaceNamesDay5: ['زيارة حديقة النباتات + زيارة جبل كنتامي'],
+            visitingPlaceNamesDay6: ['زيارة جبل و بحيرة بانور و الجلوس في احد الكافيهات الجميلة'],
+            visitingPlaceNamesDay7: ['زيارة حديقة القهوة والشوكولاتة + زيارة Alas Harum اجمل مزرعة قهوة في بالي'],
+            visitingPlaceNamesDay8: ['يوم مفتوج للراحة والتسوق'],
+            visitingPlaceNamesDay9: ['الدبابات الرباعية وسط الطبيعة والشلالات + زيارة شلال تيغينونغان الجميل'],
+            visitingPlaceNamesDay10: ['زيارة شلال TEGENUNGAN + زيارة شلال GIT GIT'],
+            visitingPlaceNamesDay11: ['PRAVARSA SWING + ALAS HARUM + TIS CAFE'],
+        },
+
+
+        guys: {
+            visitingPlaceNamesDay1: [''],
+            visitingPlaceNamesDay2: ['الدبابات الرباعية وسط الطبيعة والشلالات + زيارة شلال تيغينونغان الجميل'],
+            visitingPlaceNamesDay3: ['زيارة منتزه ALOHA مراجيح الهواء + سنتر اوبود للغدا حسب الرغبة'],
+            visitingPlaceNamesDay4: ['زيارة بحيرة براتان الفاتنة واخذ جولة بالقارب + زيارة شمال الجزيرة المعروفة بأجوائها الساحرة'],
+            visitingPlaceNamesDay5: ['زيارة بوابة منتجع هندارا الشهيرة + زيارة بحيرتين التوام'],
+            visitingPlaceNamesDay6: ['زيارة حديقة النباتات + زيارة جبل كنتامي'],
+            visitingPlaceNamesDay7: ['زيارة جبل و بحيرة بانور و الجلوس في احد الكافيهات الجميلة'],
+            visitingPlaceNamesDay8: ['زيارة حديقة القهوة والشوكولاتة + زيارة Alas Harum اجمل مزرعة قهوة في بالي'],
+            visitingPlaceNamesDay9: ['يوم مفتوج للراحة والتسوق'],
+            visitingPlaceNamesDay10: ['زيارة شلال TEGENUNGAN + زيارة شلال GIT GIT'],
+            visitingPlaceNamesDay11: ['PRAVARSA SWING + TIS CAFE'],
+        },
+
+
+        family: {
+            visitingPlaceNamesDay1: [''],
+            visitingPlaceNamesDay2: ['زيارة بحيرة براتان الفاتنة واخذ جولة بالقارب + زيارة شمال الجزيرة المعروفة بأجوائها الساحرة'],
+            visitingPlaceNamesDay3: ['الدبابات الرباعية وسط الطبيعة والشلالات + زيارة شلال تيغينونغان الجميل'],
+            visitingPlaceNamesDay4: ['زيارة منتزه ALOHA مراجيح الهواء + سنتر اوبود للغدا حسب الرغبة'],
+            visitingPlaceNamesDay5: ['زيارة بوابة منتجع هندارا الشهيرة + زيارة بحيرتين التوام'],
+            visitingPlaceNamesDay6: ['زيارة حديقة النباتات + زيارة جبل كنتامي'],
+            visitingPlaceNamesDay7: ['زيارة جبل و بحيرة بانور و الجلوس في احد الكافيهات الجميلة'],
+            visitingPlaceNamesDay8: ['زيارة حديقة القهوة والشوكولاتة + زيارة Alas Harum اجمل مزرعة قهوة في بالي'],
+            visitingPlaceNamesDay9: ['يوم مفتوج للراحة والتسوق'],
+            visitingPlaceNamesDay10: ['زيارة شلال TEGENUNGAN + زيارة شلال GIT GIT'],
+            visitingPlaceNamesDay11: ['PRAVARSA SWING + ALAS HARUM + TIS CAFE'],
+        },
+
+
+        twopeople: {
+            visitingPlaceNamesDay1: [''],
+            visitingPlaceNamesDay2: ['زيارة بحيرة براتان الفاتنة واخذ جولة بالقارب + زيارة شمال الجزيرة المعروفة بأجوائها الساحرة'],
+            visitingPlaceNamesDay3: ['الدبابات الرباعية وسط الطبيعة والشلالات + زيارة شلال تيغينونغان الجميل'],
+            visitingPlaceNamesDay4: ['زيارة منتزه ALOHA مراجيح الهواء + سنتر اوبود للغدا حسب الرغبة'],
+            visitingPlaceNamesDay5: ['زيارة بوابة منتجع هندارا الشهيرة + زيارة بحيرتين التوام'],
+            visitingPlaceNamesDay6: ['زيارة حديقة النباتات + زيارة جبل كنتامي'],
+            visitingPlaceNamesDay7: ['زيارة جبل و بحيرة بانور و الجلوس في احد الكافيهات الجميلة'],
+            visitingPlaceNamesDay8: ['زيارة حديقة القهوة والشوكولاتة + زيارة Alas Harum اجمل مزرعة قهوة في بالي'],
+            visitingPlaceNamesDay9: ['يوم مفتوج للراحة والتسوق'],
+            visitingPlaceNamesDay10: ['زيارة شلال TEGENUNGAN + زيارة شلال GIT GIT'],
+            visitingPlaceNamesDay11: ['PRAVARSA SWING + ALAS HARUM + TIS CAFE'],
+        },
+
+    },
+
+
+
+
+
+    /* Jakarta object[2] */
+    {
+
+        honeymoon: {
+            visitingPlaceNamesDay1: [''],
+            visitingPlaceNamesDay2: ['زيارة بحر وملاهي انشول + زيارة كافي سكاي + الذهاب لمطعم السدة للعشاء'],
+            visitingPlaceNamesDay3: ['زيارة ملاهي دونيا فانتاستيك + زيارة اكواريوم'],
+            visitingPlaceNamesDay4: ['يوم مفتوج للراحة والتسوق'],
+            visitingPlaceNamesDay5: ['زيارة بحر وملاهي انشول + زيارة كافي سكاي + الذهاب لمطعم السدة للعشاء'],
+            visitingPlaceNamesDay6: ['يوم مفتوج للراحة والإستجمام'],
+            visitingPlaceNamesDay7: ['يوم مفتوج للراحة والتسوق'],
+        },
+
+
+        guys: {
+            visitingPlaceNamesDay1: [''],
+            visitingPlaceNamesDay2: ['زيارة بحر وملاهي انشول + زيارة كافي سكاي + الذهاب لمطعم السدة للعشاء'],
+            visitingPlaceNamesDay3: ['زيارة ملاهي دونيا فانتاستيك + زيارة اكواريوم'],
+            visitingPlaceNamesDay4: ['يوم مفتوج للراحة والتسوق'],
+            visitingPlaceNamesDay5: ['زيارة بحر وملاهي انشول + زيارة كافي سكاي + الذهاب لمطعم السدة للعشاء'],
+            visitingPlaceNamesDay6: ['يوم مفتوج للراحة والإستجمام'],
+            visitingPlaceNamesDay7: ['يوم مفتوج للراحة والتسوق'],
+        },
+
+
+        family: {
+            visitingPlaceNamesDay1: [''],
+            visitingPlaceNamesDay2: ['زيارة بحر وملاهي انشول + زيارة كافي سكاي + الذهاب لمطعم السدة للعشاء'],
+            visitingPlaceNamesDay3: ['زيارة ملاهي دونيا فانتاستيك + زيارة اكواريوم'],
+            visitingPlaceNamesDay4: ['يوم مفتوج للراحة والتسوق'],
+            visitingPlaceNamesDay5: ['زيارة بحر وملاهي انشول + زيارة كافي سكاي + الذهاب لمطعم السدة للعشاء'],
+            visitingPlaceNamesDay6: ['يوم مفتوج للراحة والإستجمام'],
+            visitingPlaceNamesDay7: ['يوم مفتوج للراحة والتسوق'],
+        },
+
+
+        twopeople: {
+            visitingPlaceNamesDay1: [''],
+            visitingPlaceNamesDay2: ['زيارة بحر وملاهي انشول + زيارة كافي سكاي + الذهاب لمطعم السدة للعشاء'],
+            visitingPlaceNamesDay3: ['زيارة ملاهي دونيا فانتاستيك + زيارة اكواريوم'],
+            visitingPlaceNamesDay4: ['يوم مفتوج للراحة والتسوق'],
+            visitingPlaceNamesDay5: ['زيارة بحر وملاهي انشول + زيارة كافي سكاي + الذهاب لمطعم السدة للعشاء'],
+            visitingPlaceNamesDay6: ['يوم مفتوج للراحة والإستجمام'],
+            visitingPlaceNamesDay7: ['يوم مفتوج للراحة والتسوق'],
+        },
+
+    },
+
+
+
+
+
+
+
+
+    /* Puncak object[3] */
+    {
+
+        honeymoon: {
+            visitingPlaceNamesDay1: [''],
+            visitingPlaceNamesDay2: ['زيارة حديقة الجنة + زيارة حديقة شيموري'],
+            visitingPlaceNamesDay3: ['زيارة حديقة تامان سفاري (تحتاج لنهار كامل) + زيارة كافي DAMAR LANGIT'],
+            visitingPlaceNamesDay4: ['زيارة كافي NICOLES + زيارة حديقة ميلريمبا'],
+            visitingPlaceNamesDay5: ['زيارة حديقة WISATA ALAM SEVILLAGE + زيارة مطل البرشوات'],
+            visitingPlaceNamesDay6: ['زيارة كافي ROOF PARK + زيارة حديقة الزهور'],
+            visitingPlaceNamesDay7: ['زيارة بحيرة فينيسيا + زيارة منتزة THE NICE FANTASTIC'],
+            visitingPlaceNamesDay8: ['زيارة الشلالات السبعة + زيارة بحيرة تلاقا سات'],
+            visitingPlaceNamesDay9: ['يوم مفتوج للراحة والتسوق'],
+        },
+
+
+        guys: {
+            visitingPlaceNamesDay1: [''],
+            visitingPlaceNamesDay2: ['زيارة حديقة الجنة + زيارة حديقة شيموري'],
+            visitingPlaceNamesDay3: ['زيارة حديقة تامان سفاري (تحتاج لنهار كامل) + زيارة كافي DAMAR LANGIT'],
+            visitingPlaceNamesDay4: ['زيارة كافي NICOLES + زيارة حديقة ميلريمبا'],
+            visitingPlaceNamesDay5: ['زيارة حديقة WISATA ALAM SEVILLAGE + زيارة مطل البرشوات'],
+            visitingPlaceNamesDay6: ['زيارة كافي ROOF PARK + زيارة حديقة الزهور'],
+            visitingPlaceNamesDay7: ['زيارة بحيرة فينيسيا + زيارة منتزة THE NICE FANTASTIC'],
+            visitingPlaceNamesDay8: ['زيارة الشلالات السبعة + زيارة بحيرة تلاقا سات'],
+            visitingPlaceNamesDay9: ['يوم مفتوج للراحة والتسوق'],
+        },
+
+
+        family: {
+            visitingPlaceNamesDay1: [''],
+            visitingPlaceNamesDay2: ['زيارة حديقة الجنة + زيارة حديقة شيموري'],
+            visitingPlaceNamesDay3: ['زيارة حديقة تامان سفاري (تحتاج لنهار كامل) + زيارة كافي DAMAR LANGIT'],
+            visitingPlaceNamesDay4: ['زيارة كافي NICOLES + زيارة حديقة ميلريمبا'],
+            visitingPlaceNamesDay5: ['زيارة حديقة WISATA ALAM SEVILLAGE + زيارة مطل البرشوات'],
+            visitingPlaceNamesDay6: ['زيارة كافي ROOF PARK + زيارة حديقة الزهور'],
+            visitingPlaceNamesDay7: ['زيارة بحيرة فينيسيا + زيارة منتزة THE NICE FANTASTIC'],
+            visitingPlaceNamesDay8: ['زيارة الشلالات السبعة + زيارة بحيرة تلاقا سات'],
+            visitingPlaceNamesDay9: ['يوم مفتوج للراحة والتسوق'],
+        },
+
+
+        twopeople: {
+            visitingPlaceNamesDay1: [''],
+            visitingPlaceNamesDay2: ['زيارة حديقة الجنة + زيارة حديقة شيموري'],
+            visitingPlaceNamesDay3: ['زيارة حديقة تامان سفاري (تحتاج لنهار كامل) + زيارة كافي DAMAR LANGIT'],
+            visitingPlaceNamesDay4: ['زيارة كافي NICOLES + زيارة حديقة ميلريمبا'],
+            visitingPlaceNamesDay5: ['زيارة حديقة WISATA ALAM SEVILLAGE + زيارة مطل البرشوات'],
+            visitingPlaceNamesDay6: ['زيارة كافي ROOF PARK + زيارة حديقة الزهور'],
+            visitingPlaceNamesDay7: ['زيارة بحيرة فينيسيا + زيارة منتزة THE NICE FANTASTIC'],
+            visitingPlaceNamesDay8: ['زيارة الشلالات السبعة + زيارة بحيرة تلاقا سات'],
+            visitingPlaceNamesDay9: ['يوم مفتوج للراحة والتسوق'],
+        },
+
+    },
+
+
+
+
+
+
+
+
+
+    /* Bandung object[4] */
+    {
+
+        honeymoon: {
+            visitingPlaceNamesDay1: [''],
+            visitingPlaceNamesDay2: ['زيارة مول تشيمبلاس ووك و التجول في ساحة المول و شارع الجينز'],
+            visitingPlaceNamesDay3: ['زيارة شلال ماربايا الصغير و كافي TWIG CAFE الرائع + زيارة مزرعة الخيول'],
+            visitingPlaceNamesDay4: ['زيارة منتزة DCASTELLO الجديد + زيارة السوق العائم FLOATING MARKET وحديقة RAINBOW GARDEN'],
+            visitingPlaceNamesDay5: ['الجلوس في الكافي الجميل MERCUSUAR + زيارة مول و ملاهي ترانس استديو'],
+            visitingPlaceNamesDay6: ['زيارة الكافي الجميل LOUNGE & SIERRA CAFE + زيارة حديقة KIARA ARTHA PARK'],
+            visitingPlaceNamesDay7: ['زيارة قرية الخيزران الرائعة DUSUN BAMBU + زيارة شلال CURUG CIMAHI'],
+            visitingPlaceNamesDay7: ['زيارة منتزه THE GREAT ASIA AFRICA + زيارة منطقة الكافيهات الجميلة KAWASAN PUNCLUT'],
+            visitingPlaceNamesDay8: ['يوم مفتوج للراحة والتسوق'],
+        },
+
+
+        guys: {
+            visitingPlaceNamesDay1: [''],
+            visitingPlaceNamesDay2: ['زيارة مول تشيمبلاس ووك و التجول في ساحة المول و شارع الجينز'],
+            visitingPlaceNamesDay3: ['زيارة شلال ماربايا الصغير و كافي TWIG CAFE الرائع + زيارة مزرعة الخيول'],
+            visitingPlaceNamesDay4: ['زيارة منتزة DCASTELLO الجديد + زيارة السوق العائم FLOATING MARKET وحديقة RAINBOW GARDEN'],
+            visitingPlaceNamesDay5: ['الجلوس في الكافي الجميل MERCUSUAR + زيارة مول و ملاهي ترانس استديو'],
+            visitingPlaceNamesDay6: ['زيارة الكافي الجميل LOUNGE & SIERRA CAFE + زيارة حديقة KIARA ARTHA PARK'],
+            visitingPlaceNamesDay7: ['زيارة قرية الخيزران الرائعة DUSUN BAMBU + زيارة شلال CURUG CIMAHI'],
+            visitingPlaceNamesDay7: ['زيارة منتزه THE GREAT ASIA AFRICA + زيارة منطقة الكافيهات الجميلة KAWASAN PUNCLUT'],
+            visitingPlaceNamesDay8: ['يوم مفتوج للراحة والتسوق'],
+        },
+
+
+        family: {
+            visitingPlaceNamesDay1: [''],
+            visitingPlaceNamesDay2: ['زيارة مول تشيمبلاس ووك و التجول في ساحة المول و شارع الجينز'],
+            visitingPlaceNamesDay3: ['زيارة شلال ماربايا الصغير و كافي TWIG CAFE الرائع + زيارة مزرعة الخيول'],
+            visitingPlaceNamesDay4: ['زيارة منتزة DCASTELLO الجديد + زيارة السوق العائم FLOATING MARKET وحديقة RAINBOW GARDEN'],
+            visitingPlaceNamesDay5: ['الجلوس في الكافي الجميل MERCUSUAR + زيارة مول و ملاهي ترانس استديو'],
+            visitingPlaceNamesDay6: ['زيارة الكافي الجميل LOUNGE & SIERRA CAFE + زيارة حديقة KIARA ARTHA PARK'],
+            visitingPlaceNamesDay7: ['زيارة قرية الخيزران الرائعة DUSUN BAMBU + زيارة شلال CURUG CIMAHI'],
+            visitingPlaceNamesDay7: ['زيارة منتزه THE GREAT ASIA AFRICA + زيارة منطقة الكافيهات الجميلة KAWASAN PUNCLUT'],
+            visitingPlaceNamesDay8: ['يوم مفتوج للراحة والتسوق'],
+        },
+
+
+        twopeople: {
+            visitingPlaceNamesDay1: [''],
+            visitingPlaceNamesDay2: ['زيارة مول تشيمبلاس ووك و التجول في ساحة المول و شارع الجينز'],
+            visitingPlaceNamesDay3: ['زيارة شلال ماربايا الصغير و كافي TWIG CAFE الرائع + زيارة مزرعة الخيول'],
+            visitingPlaceNamesDay4: ['زيارة منتزة DCASTELLO الجديد + زيارة السوق العائم FLOATING MARKET وحديقة RAINBOW GARDEN'],
+            visitingPlaceNamesDay5: ['الجلوس في الكافي الجميل MERCUSUAR + زيارة مول و ملاهي ترانس استديو'],
+            visitingPlaceNamesDay6: ['زيارة الكافي الجميل LOUNGE & SIERRA CAFE + زيارة حديقة KIARA ARTHA PARK'],
+            visitingPlaceNamesDay7: ['زيارة قرية الخيزران الرائعة DUSUN BAMBU + زيارة شلال CURUG CIMAHI'],
+            visitingPlaceNamesDay7: ['زيارة منتزه THE GREAT ASIA AFRICA + زيارة منطقة الكافيهات الجميلة KAWASAN PUNCLUT'],
+            visitingPlaceNamesDay8: ['يوم مفتوج للراحة والتسوق'],
+        },
+
+    },
+
+
+
+
+
+
+
+
+
+    /* Lombok object[5] */
+    {
+
+        honeymoon: {
+            visitingPlaceNamesDay1: [''],
+        },
+
+
+        guys: {
+            visitingPlaceNamesDay1: [''],
+        },
+
+
+        family: {
+            visitingPlaceNamesDay1: [''],
+        },
+
+
+        twopeople: {
+            visitingPlaceNamesDay1: [''],
+        },
+
+    },
+
+
+    
+];
+
+
+
